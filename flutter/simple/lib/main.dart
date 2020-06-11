602fb857f076224ef149d815a7b0f502a6e2e006
suleymanovtat <suleymanovtat@gmail.com>
2020-06-11 18:01:50 +0300

    StatelessWidget

--- flutter/simple/lib/main.dart
+++ flutter/simple/lib/main.dart
@@ -1,8 +1,40 @@
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyFirstApp());
+void main() => runApp(MyFirstAppStatelessWidget());
+
+class MyFirstAppStatelessWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text("Home"),
+      ),
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: LinearProgressIndicator(
+              value: 5,
+            ),
+          ),
+          Text(
+            "25 %",
+            style: TextStyle(color: Colors.red, fontSize: 24),
+          ),
+          LinearProgressIndicator(),
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: Text(
+              "Loading...",
+            ),
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(onPressed: null, child: Icon(Icons.local_drink),),
+    ));
+  }
 }
 
 class MyFirstApp extends StatelessWidget {
