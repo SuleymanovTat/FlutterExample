589385ef642f82154d135d3ebfd792be8290cd35
suleymanovtat <suleymanovtat@gmail.com>
2020-06-18 08:08:43 +0300

    style item list

--- flutter/simple/lib/staticList.dart
+++ flutter/simple/lib/staticList.dart
@@ -1,5 +1,14 @@
 import 'package:flutter/material.dart';
 
+List<Widget> list = [
+  Text("Book1", style: TextStyle(fontSize: 14),),
+  Divider(),
+  Text("Book2", style: TextStyle(backgroundColor: Colors.blue),),
+  Divider(color: Colors.red,),
+  Text("book3"),
+  Divider(),
+];
+
 class StaticList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -9,14 +18,7 @@ class StaticList extends StatelessWidget {
             title: Text("StaticList"),
           ),
           body: ListView(
-            children: <Widget>[
-              Text("book1"),
-              Divider(),
-              Text("book2"),
-              Divider(),
-              Text("book3"),
-              Divider(),
-            ],
+            children: list,
           )),
     );
   }
